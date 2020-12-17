e71920ccf87badb398e42408fc39e3393bf678fc
daichi.tsuna <dt.fermat125@gmail.com>
2020-12-17 12:24:55 +0900

    remove example_snhyd

--- example_snhyd/f/eruptPara.d
+++
@@ -1,2 +0,0 @@
-TimeToCC InjectedEnergy InjectDuration
-94672800.0 1.5e+47 1000.0
--- example_snhyd/f/para1.d
+++
@@ -1,9 +0,0 @@
-ISTART, IHYDM,  WINDOW, INTERVAL TO LOOK, idev
-0    9999999      10        10000          11
-MASS CUT    COURNT NUMBER   EEXPL       Nenv
-0.0           0.4D0      0     0
-TIME TO OUTPUT 2.3E5, 3E5, 4.32E5,
-4, 4.d5, 7.d5, 1.d6, 1.1d8
-FILE NAMES TO OUTPUT
-snhydOutput/hyd.d
-snhydOutput/lc.d
