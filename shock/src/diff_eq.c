8667ebf4c93acc7dfe282b200139da8a04d44a04
Yuki Takei <takei@resceu.s.u-tokyo.ac.jp>
2021-02-12 13:33:53 +0900

    flux-limited diffusion approx. implemented.

--- shock/src/diff_eq.c
+++ shock/src/diff_eq.c
@@ -28,7 +28,7 @@ void diff_eq(double x, double y[], double dydx[])
 
 double func_dTdr(double y[], double kappa, double dTdr)
 {
-	double R = 4.*dTdr/(kappa*y[1]*y[2]);
+	double R = 4.*fabs(dTdr/(kappa*y[1]*y[2]));
 	double lambda = (2.+R)/(6.+3.*R+R*R);
 
 	return -kappa*y[1]/(4.*(P_A)*(P_C)*pow(y[2], 3.)*lambda)*y[3];
