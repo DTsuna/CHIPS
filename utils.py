5bc1ebdac547912581c2082c332a66172e514704
daichi.tsuna <dt.fermat125@gmail.com>
2021-04-26 21:54:21 +0900

    utils.py: edit shock capture

--- utils.py
+++ utils.py
@@ -118,7 +118,7 @@ def remesh_CSM(rmax, CSM_in, CSM_out, data_file_at_mass_eruption, Ncell=1000, an
 			# can be the boundary of the star and the CSM.
 			slope = [r/rho_in[i]*(rho_in[i+1]-rho_in[i])/(r_in[i+1]-r_in[i]) for i,r in enumerate(r_in) if i<len(r_in)-1]
 			istop = max([i for i in range(len(slope[:-100])) if slope[i]<-15 and slope[i+100]>-2.0 and slope[i+100]<-1.0])
-			istop += 100
+			istop += 5 
 			rstop = r_in[istop]
 			popt, pcov = curve_fit(CSMprof_func, np.log(r_in[istop:]), np.log(rho_in[istop:]), p0=[1e15,1e-15,2.0])
 			(r_break, rho_break, yrho) = (popt[0], popt[1], popt[2])
