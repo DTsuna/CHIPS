5cdae69fa728205e03037e9dbca020c075fb017a
daichi.tsuna <dt.fermat125@gmail.com>
2021-08-11 12:19:52 -0700

    saha.c: avoid pow and exp in while loop for speed

--- src/LC/shock/src/saha.c
+++ src/LC/shock/src/saha.c
@@ -49,18 +49,28 @@ void get_num_density(double rho, double T,  double ndens[])
 
 	mu_tmp[0] = 0.5;
 	mu_tmp[1] = 1.;
+	// precompute Boltzmann factors for speed
+	Boltz_HI = exp(-CHI_HI/(P_K*T));
+	Boltz_HeI = exp(-CHI_HeI/(P_K*T));
+	Boltz_HeII = exp(-CHI_HeII/(P_K*T));
+	x = 2.*M_PI*P_E*P_K*T/(P_H*P_H);
+	x_to_3ov2 = x * sqrt(x);
+
 	while(fabs(mu_tmp[1]-mu_tmp[0]) > 1.e-15){
 		mu_tmp[0] = mu_tmp[1];
-		x = 2.*M_PI*P_E*P_K*T/(P_H*P_H);
 		n_H = X*rho/MH;
 		n_He = Y/4.*rho/MH;
 		n_e = rho/(mu_tmp[0]*MH)-(X+Y/4.)*rho/MH;
-		n_HII = pow(x, 1.5)*exp(-CHI_HI/(P_K*T))/(n_e+pow(x, 1.5)*exp(-CHI_HI/(P_K*T)))*n_H;
+		//n_HII = pow(x, 1.5)*exp(-CHI_HI/(P_K*T))/(n_e+pow(x, 1.5)*exp(-CHI_HI/(P_K*T)))*n_H;
+		n_HII = x_to_3ov2 * Boltz_HI / (n_e + x_to_3ov2 * Boltz_HI) * n_H;
 		n_HI = n_H-n_HII;
-		n_HeI = pow(1.+4./n_e*pow(x, 1.5)*exp(-CHI_HeI/(P_K*T))+4./(n_e*n_e)*pow(x, 3.)*exp(-(CHI_HeI+CHI_HeII)/(P_K*T)), -1.)*n_He;
-		n_HeII = 4.*n_HeI/n_e*pow(x, 1.5)*exp(-CHI_HeI/(P_K*T));
-		n_HeIII = n_HeII/n_e*pow(x, 1.5)*exp(-CHI_HeII/(P_K*T));
-		mu_tmp[1] = pow(X*(1.+n_HII/n_H)+Y/4.*(1.+n_HeII/n_He+2.*n_HeIII/n_He), -1.);
+		//n_HeI = pow(1.+4./n_e*pow(x, 1.5)*exp(-CHI_HeI/(P_K*T))+4./(n_e*n_e)*pow(x, 3.)*exp(-(CHI_HeI+CHI_HeII)/(P_K*T)), -1.)*n_He;
+		//n_HeII = 4.*n_HeI/n_e*pow(x, 1.5)*exp(-CHI_HeI/(P_K*T));
+		//n_HeIII = n_HeII/n_e*pow(x, 1.5)*exp(-CHI_HeII/(P_K*T));
+		n_HeI = n_He / (1.+4./n_e*x_to_3ov2*Boltz_HeI+4./(n_e*n_e)*x_to_3ov2*x_to_3ov2*Boltz_HeI*Boltz_HeII);
+		n_HeII = 4.*n_HeI/n_e* x_to_3ov2 * Boltz_HeI;
+		n_HeIII = n_He - n_HeI - n_HeII;
+		mu_tmp[1] = 1./ (X*(1.+n_HII/n_H)+Y/4.*(1.+n_HeII/n_He+2.*n_HeIII/n_He));
 		mu_tmp[1] = (mu_tmp[1]+mu_tmp[0])/2.;
 		mu_tmp[1] = (mu_tmp[1]+mu_tmp[0])/2.;
 	}
