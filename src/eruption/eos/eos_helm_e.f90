152945d21767104261460630e7e15407e3c2e176
kotakatoshi <81604468+kotakatoshi@users.noreply.github.com>
2021-09-11 19:28:00 +0900

    Update eos_helm_e.f90
    delete iontable

--- src/eruption/eos/eos_helm_e.f90
+++ src/eruption/eos/eos_helm_e.f90
@@ -264,11 +264,6 @@
                        dd,dd2,ddi,dd2i,dd3i
 
 
-! open the file (use softlinks to input the desired table)
-
-       open(unit=19,file='helm_iontable.dat',status='old')
-
-
 ! for the standard table
        tion_lo   = 3.0d0
        tion_hi   = 13.0d0
